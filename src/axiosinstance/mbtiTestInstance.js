9612c575981ecb2422b2b31bb94df5f6bb055e84
jungminji <wjdalswl0215@gmail.com>
2024-09-11 17:47:56 +0900

    [deploy] json-server 연결

--- src/axiosinstance/mbtiTestInstance.js
+++ src/axiosinstance/mbtiTestInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const mbtiTestInstance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://carpal-chestnut-blinker.glitch.me",
 });
 
 export default mbtiTestInstance;
